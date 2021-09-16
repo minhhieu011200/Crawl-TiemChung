d96fddb55c042281dafb96b47ffe0ede35d68ae4
Minh Hieu <minhhieu0112000@gmail.com>
2021-09-16 16:53:58 +0700

    bootbot

--- index.js
+++ index.js
@@ -1,57 +1,39 @@
-// const axios = require('axios');
-// const path = require('path');
-// const https = require('https');
-// const rootCas = require('ssl-root-cas').create();
-
-// rootCas.addFile(path.resolve('path', 'intermediate.pem'));
-// const httpsAgent = new https.Agent({ ca: rootCas });
-
-
-// var rootCas = require('ssl-root-cas').create();
-// rootCas.addFile(path.join('public', 'intermediate.pem'))
-
-// fixes ALL https requests (whether using https directly or the request module)
-// require('https').globalAgent.options.ca = rootCas;
-// const express = require('express')
-// const app = express()
-// const cors = require("cors")
-// const axios = require("axios");
-// axios.get('https://hieusuper20hcm.netlify.app/')
-//     .then(function (response) {
-//         console.log(response);
-//     })
-//     .catch(function (error) {
-//         console.log(error);
-//     });
-// const path = require("path");
-// const https = require("https");
-// const rootCas = require("ssl-root-cas").create();
-
-// rootCas.addFile(path.resolve("public", "intermediate.pem"));
-// const httpsAgent = new https.Agent({ ca: rootCas });
-// const cheerio = require('cheerio')
-
 const puppeteer = require('puppeteer');
-const queryString = require("query-string");
+require('dotenv').config();
+const queryString = require("query-string")
+const BootBot = require("bootbot");
 
-var str = "Bùi Thế Minh Hiếu - 01/12/2000 - 1 - 0938576760";
-var a = str.split("-");
+const bot = new BootBot({
+    accessToken:
+        "EAAIA637Cv5cBAP1LEbHg8TnDCI3wVWq0Wdnx3gO6hjaCURS1lGNGC3tk5neRLJaCnqdJMgpvXaFeCT6ZBP64Ivl3aOjiaKlOilk1hkAraRpF1wsuCcWzSYS5Psg9aGw6iXxEjW1ZCOZB3AYQ1INYO3preMMc0cGDPQ7VAdAyxI4ZAR2yzZCe0",
+    verifyToken: "011200",
+    appSecret: "720686a5e99e63456ca462586e3dba01"
+});
 
-let obj = { identification: "", healthInsuranceNumber: "" };
+bot.on("message", (payload, chat) => {
+    var str = "Bùi Thế Minh Hiếu - 01/12/2000 - 1 - 0938576760";
+    var a = str.split("-");
 
-obj.fullname = a[0].trim();
-var day = a[1].trim().split("/");
-let birthday = new Date(`${day[1]}/${day[0]}/${day[2]}, 7:00:00`);
-obj.birthday = birthday.getTime();
-obj.genderId = a[2].trim();
-obj.personalPhoneNumber = a[3].trim();
+    let obj = { identification: "", healthInsuranceNumber: "" };
 
-const filter = "?" + queryString.stringify(obj);
-(async () => {
-    const browser = await puppeteer.launch({ args: ['--no-sandbox'], headless: true });
-    const page = await browser.newPage();
-    await page.goto('https://tiemchungcovid19.gov.vn/api/vaccination/public/otp-search' + filter);
+    obj.fullname = a[0].trim();
+    var day = a[1].trim().split("/");
+    let birthday = new Date(`${day[1]}/${day[0]}/${day[2]}, 7:00:00`);
+    obj.birthday = birthday.getTime();
+    obj.genderId = a[2].trim();
+    obj.personalPhoneNumber = a[3].trim();
 
-    console.log(await page.title())
-    await browser.close();
-})();
+    const filter = "?" + queryString.stringify(obj);
+    (async () => {
+        const browser = await puppeteer.launch({ args: ['--no-sandbox'], headless: true });
+        const page = await browser.newPage();
+        await page.goto('https://tiemchungcovid19.gov.vn/api/vaccination/public/otp-search' + filter);
+
+        console.log(await page.title())
+        await browser.close();
+    })();
+    chat.say("Hey, user. I got your message!");
+    const text = payload.message.text;
+});
+
+bot.start(process.env.PORT || 3000);
